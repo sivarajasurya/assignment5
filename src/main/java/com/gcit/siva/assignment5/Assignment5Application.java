198e197a60dcc1a99f2792ecd79ee1cb89776bb8
Siva Raja Surya <sivarajasurya.elangovan@gcitsolutions.com>
2022-10-19 14:14:11 +0530

    1

--- src/main/java/com/gcit/siva/assignment5/Assignment5Application.java
+++ src/main/java/com/gcit/siva/assignment5/Assignment5Application.java
@@ -1,20 +1,11 @@
 package com.gcit.siva.assignment5;
 
-import com.gcit.siva.Assignment4;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class Assignment5Application {
-
-
 	public static void main(String[] args)  {
 		SpringApplication.run(Assignment5Application.class, args);
-
-
 	}
-
 }
--- src/main/java/com/gcit/siva/assignment5/spring/Spring.java
+++ src/main/java/com/gcit/siva/assignment5/spring/Spring.java
@@ -1,27 +1,20 @@
 package com.gcit.siva.assignment5.spring;
 
-
 import com.gcit.siva.Assignment4;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
-
 @RestController
 public class Spring {
-
-
-    @GetMapping("/country/searchByName")
-    @ResponseBody
-    public static String usingCountryName(@RequestParam String id) {
-        return usingCountryName(id);
-
+    private Assignment4 assignment4;
+    public Spring() throws IOException {
+      assignment4= new Assignment4();
     }
-
-    @GetMapping("/country/searchByCode")
-    @ResponseBody
-    public static String usingCountryCode(@RequestParam String id) {
-        return usingCountryCode(id);
-
+    @RequestMapping(path = "/country/searchByName/{name}", method = RequestMethod.GET)
+    public  String usingCountryName(@PathVariable String name) {
+        return assignment4.usingCountryName(name);
+    }
+    @RequestMapping(path = "/country/searchByCode/{code}" , method = RequestMethod.GET)
+    public String usingCountryCode(@PathVariable String code) {
+        return assignment4.usingCountryCode(code);
     }
 }
