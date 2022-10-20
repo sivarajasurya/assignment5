c14cc52feac7075f6b8a162169c83d5bea9dcfde
Siva Raja Surya <sivarajasurya.elangovan@gcitsolutions.com>
2022-10-20 11:00:38 +0530

    1

--- src/main/java/com/gcit/siva/assignment5/web/Web.java
+++ src/main/java/com/gcit/siva/assignment5/web/Web.java
@@ -1,13 +1,13 @@
-package com.gcit.siva.assignment5.spring;
+package com.gcit.siva.assignment5.web;
 
 import com.gcit.siva.Assignment4;
 import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 @RestController
-public class Spring {
+public class Web {
     private Assignment4 assignment4;
-    public Spring() throws IOException {
-      assignment4= new Assignment4();
+    public Web() throws IOException {
+      assignment4 = new Assignment4();
     }
     @RequestMapping(path = "/country/searchByName/{name}", method = RequestMethod.GET)
     public  String usingCountryName(@PathVariable String name) {
